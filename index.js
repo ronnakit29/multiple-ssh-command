37da49842580c801d3034cd741efb294a9603ad8
ronnakit29 <miku.fc.net@gmail.com>
2023-01-05 12:49:00 +0700

    fixed stop process

--- index.js
+++ index.js
@@ -57,13 +57,22 @@ function createConnection(host, username, password, command) {
 const fs = require("fs");
 const hostTextFile = fs.readFileSync("./hosts.txt", "utf8");
 const passwordTextFile = fs.readFileSync("./password.txt", "utf8");
-const hosts = hostTextFile.split("\r\n").map((host, index) => {
+let hosts = hostTextFile.split("\r\n").map((host, index) => {
   return {
     host,
     username: "root",
     password: passwordTextFile.split("\r\n")[index],
   };
 });
+
+function bypassHost(host) {
+  // save host to bypass.txt
+  fs.appendFile("bypass.txt", host + "\r\n", function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+  });
+}
+
 // log => host
 console.log(
   "\x1b[32m",
@@ -102,79 +111,102 @@ async function main() {
             rl.close();
             return;
           }
-          // ask show output
-          rl.question("Show output (y/n) : ", async (showOutput) => {
-            // run command
-            let success = 0;
-            let fail = 0;
-            const successHost = [];
-            const failHost = [];
-            for (let i = 0; i < hosts.length; i++) {
-              try {
-                console.log(
-                  "No.",
-                  i + 1,
-                  "[Host]",
-                  hosts[i].host,
-                  "Sending Command...!"
-                );
-                const data = await createConnection(
-                  hosts[i].host,
-                  hosts[i].username,
-                  hosts[i].password,
-                  command
-                );
-                success++;
-                successHost.push(hosts[i].host);
-                if (showOutput === "y") {
-                  console.log(data.toString());
-                }
-              } catch (error) {
-                console.log("Host", hosts[i].host, "Sent Command Error...!");
-                fail++;
-                failHost.push(hosts[i].host);
+          const bypassTextFile = fs.readFileSync("./bypass.txt", "utf8");
+          const bypassHosts = bypassTextFile.split("\r\n");
+          //   ask bypass host
+          rl.question(
+            "Bypass host [" + bypassHosts.length + "] (y/n) : ",
+            async (bypass) => {
+              if (bypass === "y") {
+                // read bypass.txt
+
+                // filter bypass host
+                const filterHosts = hosts.filter((host) => {
+                  return !bypassHosts.includes(host.host);
+                });
+                // set hosts
+                hosts = filterHosts;
               }
+              // ask show output
+              rl.question("Show output (y/n) : ", async (showOutput) => {
+                // run command
+                let success = 0;
+                let fail = 0;
+                const successHost = [];
+                const failHost = [];
+                for (let i = 0; i < hosts.length; i++) {
+                  try {
+                    console.log(
+                      "No.",
+                      i + 1,
+                      "[Host]",
+                      hosts[i].host,
+                      "Sending Command...!"
+                    );
+                    const data = await createConnection(
+                      hosts[i].host,
+                      hosts[i].username,
+                      hosts[i].password,
+                      command
+                    );
+                    success++;
+                    successHost.push(hosts[i].host);
+                    if (showOutput === "y") {
+                      console.log(data.toString());
+                    }
+                  } catch (error) {
+                    console.log(
+                      "Host",
+                      hosts[i].host,
+                      "Sent Command Error...!"
+                    );
+                    bypassHost(hosts[i].host);
+                    fail++;
+                    failHost.push(hosts[i].host);
+                  }
+                }
+                console.log("Done all!");
+                // count all
+                console.log("Total Host : ", hosts.length);
+                console.log(
+                  "REPORT, success: [",
+                  "\x1b[32m",
+                  success,
+                  "\x1b[0m",
+                  "] fail: [",
+                  "\x1b[31m",
+                  fail,
+                  "\x1b[0m",
+                  "]"
+                );
+                // green
+                console.log(
+                  "\x1b[32m",
+                  "================== SUCCESS ==================",
+                  "\x1b[0m"
+                );
+                // reset
+                successHost.forEach((host) => {
+                  // yellow before host
+                  console.log("==> ", "\x1b[33m", host, "\x1b[0m");
+                });
+                // red
+                console.log(
+                  "\x1b[31m",
+                  "================== FAIL ==================",
+                  "\x1b[0m"
+                );
+                failHost.forEach((host) => {
+                  // yellow before host
+                  console.log("==> ", "\x1b[33m", host, "\x1b[0m");
+                });
+                // reset color
+                console.log("\x1b[0m");
+                rl.close();
+                return;
+              });
             }
-            console.log("Done all!");
-            // count all
-            console.log("Total Host : ", hosts.length);
-            console.log(
-              "REPORT, success: [",
-              "\x1b[32m",
-              success,
-              "\x1b[0m",
-              "] fail: [",
-              "\x1b[31m",
-              fail,
-              "\x1b[0m",
-              "]"
-            );
-            // green
-            console.log(
-              "\x1b[32m",
-              "================== SUCCESS ==================",
-              "\x1b[0m"
-            );
-            // reset
-            successHost.forEach((host) => {
-              // yellow before host
-              console.log("==> ", "\x1b[33m", host, "\x1b[0m");
-            });
-            // red
-            console.log(
-              "\x1b[31m",
-              "================== FAIL ==================",
-              "\x1b[0m"
-            );
-            failHost.forEach((host) => {
-              // yellow before host
-              console.log("==> ", "\x1b[33m", host, "\x1b[0m");
-            });
-            // reset color
-            console.log("\x1b[0m");
-            rl.close();
-            return;
-          });
+          );
         } else {
           rl.close();
           return;
